28941e41819e532d3bf0e18c40c621cadde241bd
divyaswami122 <130961021+divyaswami122@users.noreply.github.com>
2024-07-08 17:01:36 +0530

    Add files via upload

---
+++ examserver/src/main/java/com/exam/ExamservApplication.java
@@ -0,0 +1,81 @@
+package com.exam;
+
+import com.exam.helper.UserFoundException;
+import com.exam.model.Role;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.repo.QuizRepository;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class ExamservApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public QuizRepository quizRepository;
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(ExamservApplication.class, args);
+
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+
+
+            System.out.println("starting code");
+//
+            User user = new User();
+
+            user.setFirstName("Swami");
+            user.setLastName("Divya");
+            user.setUsername("Div@04");
+            user.setPassword(this.bCryptPasswordEncoder.encode("abc@04"));
+            user.setEmail("abc@gmail.com");
+            user.setProfile("default.png");
+
+            Role role1 = new Role();
+            role1.setRoleId(44L);
+            role1.setRoleName("ADMIN");
+
+            Set<UserRole> userRoleSet = new HashSet<>();
+            UserRole userRole = new UserRole();
+
+            userRole.setRole(role1);
+
+            userRole.setUser(user);
+
+            userRoleSet.add(userRole);
+
+            User user1 = this.userService.createUser(user, userRoleSet);
+            System.out.println(user1.getUsername());
+
+
+        } catch (UserFoundException e) {
+            e.printStackTrace();
+
+
+        }
+
+
+    }
+
+
+}

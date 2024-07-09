291658754a48c91bec738228f4b93837cb4cf746
divyaswami122 <130961021+divyaswami122@users.noreply.github.com>
2024-07-09 11:27:57 +0530

    Add files via upload

--- examserver/src/main/java/com/exam/model/User.java
+++ examserver/src/main/java/com/exam/model/User.java
@@ -5,6 +5,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,12 +20,37 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotEmpty(message = "UserName can't be Empty.")
+    @Size(min = 2, max = 30, message = "Minimum 2 and maximum 30 characters allowed.")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Only Alphabets are Allowed.")
     @Column(name="UserName",length=30)
     private String username;
+    @NotEmpty(message = "Password can't be Empty.")
+    @Size(min=8, max = 10, message = "Minimum 8 and maximum 10 characters allowed.")
+    @Pattern(regexp = "^[A-Za-z0-9\\d@$!%*?&]*$", message = "At least one lowercase letter, one uppercase letter, one digit and one spacial character must be there")
+    @Column(name="Password",length=20)
     private String password;
+    @NotEmpty(message = "FirstName can't be Empty.")
+    @Size(min = 2, max = 30, message = "Minimum 2 and maximum 30 characters allowed.")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Only Alphabets are Allowed.")
+    @Column(name="FirstName",length=30)
     private String firstName;
+    @NotEmpty(message = "LastName can't be Empty.")
+    @Size(min = 3, max = 30, message = "Minimum 3 and maximum 30 characters allowed.")
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Only Alphabets are Allowed.")
+    @Column(name="LastName",length=30)
     private String lastName;
+    @NotEmpty(message = "Email can't be Empty.")
+    @Size(max = 255, message = "Email address must not exceed 255 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$", message = "Only Alphabets and Numeric Values are Allowed")
+    @Email(message = "Please provide a valid email address")
+    @Column(name="Email",length=30)
     private String email;
+    //This regex pattern allows for variations like "(123) 456-7890", "123-456-7890", "1234567890"
+    @NotEmpty(message = "PhoneNumber can't be Empty.")
+    @Size(min=10,max =10, message = "PhoneNumber must be exactly 10 characters")
+    @Pattern(regexp = "^[0-9-()]*$", message = "Only digit are Allowed")
+    @Column(name="PhoneNumber",length=10)
     private String phone;
     private boolean enabled = true;
     private String profile;

015e1e6d767f0a87e3058b89f748231e34766ee9
divyaswami122 <130961021+divyaswami122@users.noreply.github.com>
2024-07-11 13:05:00 +0530

    Delete examserver directory

--- examserver/src/main/java/com/exam/repo/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exam.repo;
-
-import com.exam.model.exam.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- examserver/src/main/java/com/exam/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exam.repo;
-
-import com.exam.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
